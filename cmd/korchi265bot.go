0afadd7509eabc79eb4f856e53c378671d128016
Andrii Merezhko <i@gava.org.ua>
2023-05-06 20:02:06 +0300

    v1.0.2

--- cmd/korchi265bot.go
+++ cmd/korchi265bot.go
@@ -1,19 +1,27 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
+)
+
+var (
+	TeleToken = os.Getenv("TELE_TOKEN")
 )
 
 // korchi265botCmd represents the korchi265bot command
 var korchi265botCmd = &cobra.Command{
-	Use:   "korchi265bot",
-	Short: "A brief description of your command",
+	Use:     "korchi265bot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +29,32 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("korchi265bot called")
+		fmt.Printf("korchi265bot %s started", appVersion)
+
+		korchi265bot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
+			return
+		}
+
+		korchi265bot.Handle(telebot.OnText, func(m telebot.Context) error {
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm Korchi265bot %s!", appVersion))
+			}
+
+			return err
+		})
+
+		korchi265bot.Start()
 	},
 }
 
--- go.mod
+++ go.mod
@@ -3,7 +3,14 @@ module github.com/gitgav/korchi265bot
 go 1.20
 
 require (
+	github.com/spf13/cobra v1.7.0
+	gopkg.in/telebot.v3 v3.1.3
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
+	github.com/mitchellh/hashstructure v1.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tucnak/telebot v2.0.0+incompatible // indirect
 )
